e00f7e8b5b7e32db9bae41b1aa1be60b2bea5767
Umberban <romanovborysv@gmail.com>
2023-05-08 16:39:46 +0300

    project start
    Also added api requests

---
+++ src/api/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+const BASE_URL = 'https://6458fa234eb3f674df8301e9.mockapi.io/users'
+export async function getUsers(page) {
+  try {
+    const response = await axios.get(`${BASE_URL}?page=${page}&limit=4`);
+    return response.data;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getUser(id) {
+  try {
+    const response = await axios.get(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function changeFollowers(id, update) {
+  try {
+    const response = await axios.put(`${BASE_URL}/${id}`, update);
+    return response.data;
+  } catch (error) {
+    console.log(error)
+  }
+}
